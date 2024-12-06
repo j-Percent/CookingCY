eb8747b18755d6b936be46f27d2a9583f7645ed1
j-Percent <chichichen1117@hotmail.com>
2024-12-05 22:51:28 -0500

    More Setup

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Customer : MonoBehaviour
 {
     // 0 - outside; 1 - order; 2 - plate; 3 - check
     public int _state;
+
+    // 0 - no player; 1 - p1; 2 - p2
+    public int _playerState;
 
     // 1 - table 1; 2 - table 2; 3 - table 3; 4 - table 4
     public int _tableNum;
