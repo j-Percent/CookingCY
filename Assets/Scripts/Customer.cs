671b7d97d7285b926d2036bf63c6c58f39ac3d9e
j-Percent <chichichen1117@hotmail.com>
2024-12-05 22:39:48 -0500

    Basic Code Setup

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    // 0 - outside; 1 - order; 2 - plate; 3 - check
+    public int _state;
+
+    // 1 - table 1; 2 - table 2; 3 - table 3; 4 - table 4
+    public int _tableNum;
+
+    // how long customers wait until leaving (base patience is for resetting patience)
+    public float _patience;
+    public float _basePatience;
+
+    // true - is VIP; false - normal customer
+    public bool _isVIP;
+
+    // true - need service, patience starts reducing; false - don't need service, reduce service cooldown
+    public bool _serviceRequired;
+    public float _serviceCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
